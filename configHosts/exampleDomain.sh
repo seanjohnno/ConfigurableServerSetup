f9065bc0ab454aee0471225599f72e412e9308a8
Sean Johnson <s3anj@hotmail.co.uk>
2019-01-05 01:01:47 +0000

    Removing real repo

--- configHosts/exampleDomain.sh
+++ configHosts/exampleDomain.sh
@@ -2,5 +2,5 @@
 
 export NAME="exampleName"
 export PORT="8080"
-export GIT_REPOSITORY="git@github.com:kfedorov91/docker-node-hello-world.git"
+export GIT_REPOSITORY="git@github.com:someuser/docker-node-hello-world.git"
 export DOMAINS="www.exampledomain.co.uk exampledomain.co.uk"
